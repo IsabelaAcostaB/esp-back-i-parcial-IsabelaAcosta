c6033fc4807c3912aee056dc73eb86a4fc5fc11c
IsabelaAcosta <anacosta21@icloud.com>
2022-11-12 16:48:13 +0100

    Bean

--- eureka-server/src/test/java/com/dh/parcial/eurekaserver/EurekaServerApplicationTests.java
+++ eureka-server/src/test/java/com/dh/parcial/eurekaserver/EurekaServerApplicationTests.java
@@ -2,6 +2,7 @@ package com.dh.parcial.eurekaserver;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootTest
 @EnableEurekaServer
